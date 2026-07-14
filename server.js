0497b1466cdf2ae9c77253d64716bb4cf2c4a13e
alice <onlyrong000@gmail.com>
2026-07-14 22:14:32 +0800

    modify:第二版

--- server.js
+++ server.js
@@ -1,133 +1,124 @@
 const http = require('http');
 const { v4: uuidv4 } = require('uuid');
-
-// 資料
-let data = {
-  result: null,
-  datas: [],
-  msg: null,
-};
-
-const response= function (res,status,data) {
-  let headers = {
-    'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
-    'Access-Control-Allow-host': '*',
-    'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
-    'Content-Type': 'application/json'
-  }
-
-  res.writeHead(status, headers);
-  res.end(JSON.stringify(data)); //有 response 回寫及回傳
+const headers = {
+  'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
+  'Content-Type': 'application/json'
 }
-
-const resetResult = function (data){
-  return {
-    result: null,
-    datas: data.datas,
-    msg: null,
-  }
-}
+let data = [];
 
 const requestListener = function (req, res) {
-  const [host,url,id] = req.url.split('/');
-  let isMainPath = true;
-  let hasId = false;
-  let status = 200;
-  console.log('req=>', host, url, id);
-  
-  if (url !== 'todos'){
-    isMainPath = false;
-  }
-  
-  if (id) {
-    hasId = true;
-  }
-
-  data = resetResult(data);
-
+  const [host,url, id] = req.url.split('/');
+  let isMainPath = false;
   let body = '';
+
+  console.log('method=>', req.method);
+  console.log('req=>', req.url);
+  console.log('url=>', req.url.split('/'), url == 'todos');
+  console.log('id=>',id);
+
+  // 沒有找到此頁面
+  if (url == 'todos') {
+    isMainPath = true;
+  }
+
   req.on('data', chunk => { // 每來一段資料就觸發
     body += chunk
   })
 
   if (isMainPath && req.method === 'GET') { //得到所有資料
-    data.result = 'success';
-    response(res, status, data);
-  } else if (isMainPath && req.method === 'OPTIONS') {// preflight options API 檢查機制: https://developer.mozilla.org/zh-TW/docs/Web/HTTP/Guides/CORS
-    data.result = 'success';
-    data.msg = '出現 preflight options API 檢查機制';
-    response(res, status, data);
+    res.writeHead(200, headers);
+    return res.end(JSON.stringify({
+      result: 'success',
+      datas: data,
+    }))
+  } else if (isMainPath && req.method === 'OPTIONS') {// CORS預檢
+    res.writeHead(204, headers);
+    return res.end();
   } else if (isMainPath && req.method === 'POST') { // 新增資料
     req.on('end', () => { // 全部資料收完才觸發
       try {
-        const item = JSON.parse(body).title;
-        if (!item) throw new Error('未取到正確屬性');
-        data.datas.push({ id: uuidv4(), title: item });
-        data.result = 'success';
-        data.msg = '已新增一筆資料';
-        response(res, status, data);
+        const item = JSON.parse(body)?.title?.trim();
+        console.log('item', item, !item);
+        
+        if (!item) throw new Error('未取到title屬性或值');
+        console.log('sss');
+        
+        data.push({ id: uuidv4(), title: item });
+        res.writeHead(200, headers);
+        return res.end(JSON.stringify({
+          result: 'success',
+          datas: { id: uuidv4(), title: item },
+        }))
       } catch (error) {
-        status = 400;
-        data.result = 'fail';
-        data.msg = typeof error === 'string' ? error.message :'請提供 title 屬性';
-
-        response(res, status, data);
+        res.writeHead(400, headers);
+        return res.end(JSON.stringify({
+          result: 'fail',
+          msg: error.message,
+        }))
       }
     })
-
-  } else if (isMainPath && req.method === 'PATCH' && hasId){ //編輯資料
-    req.on('end',()=>{
+    return;
+  } else if (isMainPath && req.method === 'PATCH') { //編輯資料
+    req.on('end', () => {
       try {
-        const item = JSON.parse(body).title;
-        if (!item) throw new Error('未取到正確屬性');
-
-        const editIndex = data.datas.findIndex(item => item.id === id);
-        data.result = 'success';
-        data.datas[editIndex].title = item;
-        data.msg = '已成功編輯資料';
-        response(res, status, data);
+        const item = JSON.parse(body)?.title?.trim();
+        if (!item) throw new Error('未取到title屬性或值');
+        const editIndex = data.findIndex(item => item.id === id);
+        console.log('editIndex', editIndex,data);
+        if (editIndex === -1) throw new Error('查無此id');
+        data[editIndex].title = item;
+        res.writeHead(200, headers);
+        return res.end(JSON.stringify({
+          result: 'success',
+          datas: data[editIndex],
+        }))
       } catch (error) {
-        status = 400;
-        data.result = 'fail';
-        data.msg = typeof error === 'string' ? error.message : '請提供 title 屬性';
-
-        response(res, status, data);
+        res.writeHead(400, headers);
+        return res.end(JSON.stringify({
+          result: 'fail',
+          msg: error.message,
+        }))
       }
     })
-  } else if (isMainPath && req.method === 'DELETE' && hasId) { //刪除指定資料
-    const delIndex = data.datas.findIndex(item=>item.id === id);
-    if(delIndex !== -1){
-      data.datas.splice(delIndex, 1);
-      data.result = 'success';
-      data.msg = '已刪除一筆資料';
-    }else{
-      data.result = 'fail';
-      data.msg = '捕捉到未存在的id';
+    return;
+  } else if (isMainPath && req.method === 'DELETE') { //刪除指定與全部資料
+    const delIndex = data.findIndex(item => item.id === id);
+    if (delIndex !== -1) {//刪除指定
+      data.splice(delIndex, 1);
+      res.writeHead(200, headers);
+      return res.end(JSON.stringify({
+        result: 'success',
+        datas: data,
+      }))
+    } else {//刪除全部
+      if (data.length === 0) {
+        res.writeHead(400, headers);
+        return res.end(JSON.stringify({
+          result: 'fail',
+          msg: '尚未有資料，無法進行刪除',
+        }))
+      } else {
+        data = [];
+        res.writeHead(200, headers);
+        return res.end(JSON.stringify({
+          result: 'success',
+          datas: data,
+        }))
+      }
     }
-    
-    response(res, status, data);
-  } else if (isMainPath && req.method === 'DELETE') { //刪除全部資料
-    if (data.datas.length === 0){
-      data.result = 'fail';
-      data.msg = '尚未有資料，無法進行刪除';
-    }else{
-      data.result = 'success';
-      data.datas = [];
-      data.msg = '已刪除全部資料';
-    }
-    response(res, status, data);
   }
 
-  // 沒有找到此頁面
-  if (!isMainPath) {
-    status = 404;
-    data.result = 'fail';
-    data.datas = [];
-    data.msg = '沒有找到此頁面';
-    response(res, status, data);
-  }
+  res.writeHead(404, headers);
+  return res.end(JSON.stringify({
+    result: 'fail',
+    msg: '沒有找到此頁面',
+  }))
 };
 
 
 
-http.createServer(requestListener).listen(8080);
+http.createServer(requestListener).listen(8080, () => {
+  console.log('Server: http://localhost:8080');
+});
